83e8c12ce8e2b82ae3a5361d125c7f92956956cd
RussianBlue25 <horota2@outlook.jp>
2020-11-06 01:31:33 +0900

    change snake case to camel case

--- src/main.go
+++ src/main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 
 	//TODO: implement cpu
-	var fetched_binary uint32
+	var fetchedBinary uint32
 	var inst instruction.Instruction
 
 	for {
@@ -29,7 +29,7 @@ func main() {
 			break
 		}
 		fmt.Printf("%x\n", fetched_binary)
-		inst = interpret_inst(fetched_binary)
+		inst = interpretInst(fetched_binary)
 	}
 	cpu := cpu.Cpu{}
 
@@ -47,18 +47,18 @@ func main() {
 	fmt.Println(cpu.Register[inst.Rd])
 }
 
-func interpret_inst(fetched_binary uint32) (inst instruction.Instruction) {
-	opcode := int(fetched_binary & 0x0000007F)
+func interpretInst(fetched_binary uint32) (inst instruction.Instruction) {
+	opcode := int(fetcheBinary & 0x0000007F)
 	var rd int
 	var funct3 int
 	var rs1 int
 	var imm int
 
 	if opcode == 19 {
-		rd = int((fetched_binary & 0x00000F80) >> 7)
-		funct3 = int((fetched_binary & 0x00007000) >> 12)
-		rs1 = int((fetched_binary & 0x000F8000) >> 15)
-		imm = int((fetched_binary & 0xFFF00000) >> 20)
+		rd = int((fetchedBinary & 0x00000F80) >> 7)
+		funct3 = int((fetchedBinary & 0x00007000) >> 12)
+		rs1 = int((fetchedBinary & 0x000F8000) >> 15)
+		imm = int((fetchedBinary & 0xFFF00000) >> 20)
 	}
 	return instruction.Instruction{Opcode: opcode, Rd: rd, Rs1: rs1, Funct3: funct3, Imm: imm}
 }
