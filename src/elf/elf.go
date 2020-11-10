c6481d6ff3d3838ebb48e978c8bcf013ef43e743
RussianBlue25 <horota2@outlook.jp>
2020-11-10 21:48:53 +0900

    Enable to read program header

--- src/elf/elf.go
+++ src/elf/elf.go
@@ -23,6 +23,17 @@ type elf32Header struct {
     E_shstrndx  uint16
 }
 
+type elf32Pheader struct {
+    P_type      uint32
+    P_offset    uint32
+    P_vaddr     uint32
+    P_paddr     uint32
+    P_filesz    uint32
+    P_memsz     uint32
+    P_flags     uint32
+    P_align     uint32
+}
+
 func ElfLoad() {
     file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -31,12 +42,25 @@ func ElfLoad() {
     }
 
     var elf32Header elf32Header
+    var elf32Pheader [128]elf32Pheader //better way?
 
-    errb := binary.Read(file, binary.LittleEndian, &elf32Header)
-    if errb != nil {
-        fmt.Println("can't read binary")
+    for i := 0;; i++ {
+        if i == 0 {
+            errb := binary.Read(file, binary.LittleEndian, &elf32Header)
+            if errb != nil {
+                fmt.Println("can't read elf header")
+            }
+            fmt.Printf("%x\n", elf32Header)
+        } else if i <= int(elf32Header.E_phnum) {
+            errpb := binary.Read(file, binary.LittleEndian, &elf32Pheader[i-1])
+            if errpb != nil {
+                fmt.Println("can't read program header")
+            }
+            fmt.Printf("%x\n", elf32Pheader[i-1])
+        } else {
+            break
+        }
     }
-    fmt.Printf("%x\n", elf32Header)
 
     if elf32Header.E_ident[0] == 0x7f && elf32Header.E_ident[1] == 0x45 && elf32Header.E_ident[2] == 0x4c && elf32Header.E_ident[3] == 0x46 {
         fmt.Println("this is an ELF file")
@@ -51,4 +75,6 @@ func ElfLoad() {
         fmt.Println("not 32bit")
         os.Exit(1)
     }
+
+    fmt.Printf("%x\n", elf32Header.E_phnum)
 }
