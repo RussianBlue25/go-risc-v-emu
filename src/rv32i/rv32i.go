158411fbca38889bbb32f9079636600c85167671
RussianBlue25 <horota2@outlook.jp>
2020-11-11 20:57:40 +0900

    Fix type of pc

--- src/rv32i/rv32i.go
+++ src/rv32i/rv32i.go
@@ -49,8 +49,8 @@ func Srai(inst instruction.Instruction, cpu *cpu.Cpu) {
 }
 
 func Jal(inst instruction.Instruction, cpu *cpu.Cpu) {
-	cpu.Registers[inst.Rd] = int(cpu.Pc) + 4
-	cpu.Pc += uint32(inst.Imm)
+	cpu.Registers[inst.Rd] = cpu.Pc + 4
+	cpu.Pc += inst.Imm
 }
 
 
